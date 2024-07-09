f0392ec47369bb4e097980e1afdcb90909927991
Yurii Poudanien <mcpchip@gmail.com>
2024-07-09 22:34:18 +0300

    * remove old approach for UI

---
+++ app/src/main/java/com/pou/udfsample/ui/details/DetailsScreen.kt
@@ -0,0 +1,15 @@
+package com.pou.udfsample.ui.details
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.pou.udfsample.ui.main.MainViewModel
+
+@Composable
+fun MainScreen(viewModel: MainViewModel = viewModel()) {
+
+}
+
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
+}
--- app/src/main/java/com/pou/udfsample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pou/udfsample/ui/main/MainViewModel.kt
@@ -11,6 +11,22 @@ class MainViewModel(
     private val repository: FruitsRepository
 ) : ViewModel() {
 
+
+    // Expose screen UI state
+   /* private val _uiState = MutableStateFlow(DiceUiState())
+    val uiState: StateFlow<DiceUiState> = _uiState.asStateFlow()
+
+    // Handle business logic
+    fun rollDice() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                firstDieValue = Random.nextInt(from = 1, until = 7),
+                secondDieValue = Random.nextInt(from = 1, until = 7),
+                numberOfRolls = currentState.numberOfRolls + 1,
+            )
+        }
+    }*/
+
     private var currentState: State = State()
     private var rawData: List<FruitModel> = emptyList()
 
